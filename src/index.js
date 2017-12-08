8d523e5c5ee5c65eb35a8b53f4d5de809a6feb7c
Ming Liu <vmliu1@gmail.com>
2017-12-07 20:46:45 -0800

    Remove peg import from source

--- src/index.js
+++ src/index.js
@@ -6,12 +6,32 @@ export default ({ types: t }) => {
   return {
     name: 'babel-plugin-pegjs-inline-precompile',
     visitor: {
+      // Modified from https://github.com/ember-cli/babel-plugin-htmlbars-inline-precompile/blob/master/index.js#L8-L23
+      ImportDeclaration(path, state) {
+        let node = path.node;
+
+        if (t.isLiteral(node.source, { value: 'pegjs-inline-precompile' })) {
+          let first = node.specifiers && node.specifiers[0];
+
+          if (!t.isImportDefaultSpecifier(first)) {
+            let input = state.file.code;
+            let usedImportStatement = input.slice(node.start, node.end);
+            let msg = `Only \`import hbs from 'pegjs-inline-precompile'\` is supported. You used: \`${usedImportStatement}\``;
+            throw path.buildCodeFrameError(msg);
+          }
+
+          state.importId = state.importId || path.scope.generateUidIdentifierBasedOnNode(path.node.id);
+          path.scope.rename(first.local.name, state.importId.name);
+          path.remove();
+        }
+      },
+
       TaggedTemplateExpression(path, state) {
         let name = path.node.tag.name
         let quasi = path.node.quasi
         let element = quasi.quasis[0]
 
-        if (name === 'peg') {
+        if (name === state.importId.name) {
           if (quasi.expressions.length) {
             // TODO throw error here, dont support inlined expression
           }
